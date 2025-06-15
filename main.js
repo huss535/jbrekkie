d0a24eb02d79b4c21c929930fec73fbe74987028
efar716 <110000165+efar716@users.noreply.github.com>
2025-06-15 17:16:05 +1200

    Merch component + Section Created

--- main.js
+++ main.js
@@ -15,6 +15,18 @@ export function getTourBookingComponent(date, venue, location, buttonText) {
   `;
 }
 
+export function getMerchComponent(merchSrc, merchName) {
+    return `
+     <div class="merch-container">
+        <img src=${merchSrc} alt=${merchName} />
+        <span>${merchName}</span>
+        <button>Buy Now</button>
+      </div>
+  `;
+}
+
+
+//Tour Data
 const tourData = [
     { date: "January 15, 2024", venue: "Madison Square Garden", location: "New York, NY", buttonText: "Buy Tickets" },
     { date: "February 10, 2024", venue: "United Center", location: "Chicago, IL", buttonText: "Buy Tickets" },
@@ -24,6 +36,10 @@ const tourData = [
 
 ];
 
+
+
+
+// Render Tour Booking Components
 tourData.forEach((tour, index) => {
     const tourBookingComponent = getTourBookingComponent(tour.date, tour.venue, tour.location, tour.buttonText);
     document.querySelector("#tour-section").innerHTML += tourBookingComponent;
@@ -32,3 +48,18 @@ tourData.forEach((tour, index) => {
     }
 
 });
+
+// Merch Data
+const merchData = [
+    { src: "public/images/jb-merch-1.svg", name: "T-Shirt" },
+    { src: "public/images/jb-merch-1.svg", name: "Hoodie" },
+    { src: "public/images/jb-merch-1.svg", name: "Poster" },
+    { src: "public/images/jb-merch-1.svg", name: "Vinyl Record" },
+    { src: "public/images/jb-merch-1.svg", name: "Cap" },
+];
+
+merchData.forEach((merch) => {
+    const merchComponent = getMerchComponent(merch.src, merch.name);
+    document.querySelector("#merch-section").innerHTML += merchComponent;
+});
+
